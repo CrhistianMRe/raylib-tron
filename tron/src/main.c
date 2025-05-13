551c4421376eec726a98ffe9e4b937b6afaac2d7
CrhistianMRe <came.29meca@gmail.com>
2025-05-13 20:37:27 +0800

    Add function for fps and some argv argc params

--- tron/src/main.c
+++ tron/src/main.c
@@ -1,17 +1,18 @@
 #include "raylib.h"
 #include "controller.c"
 
-int main() {
+int main(int argc, char *argv[], char *argv2[]) {
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-    InitWindow(500, 500, "Demo Tron");
+    InitWindow(1000, 1000, "Demo Tron");
 
 
 
     while (!WindowShouldClose()) {
         //updateGame();  //
 
-        initGame();
+        SetTargetFPS(60);
+        initGame(&argv);
         BeginDrawing();
         ClearBackground(BLACK);
 
