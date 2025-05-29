1187d6ff3e9e7a21019409586b221d45df2c4f73
CrhistianMRe <came.29meca@gmail.com>
2025-05-29 22:38:13 +0800

    style(controller.c): organize some indenting

--- tron/src/controller.c
+++ tron/src/controller.c
@@ -50,24 +50,18 @@ void movePlayerPos(Player *player){
 void restartPlayer(){
 
     player1.position = (Vector2){500, 200};
+    player1.triangle.height = 20;
+    player1.triangle.width = 40;
+    player1.triangle.rotation = 0;
+    player1.state = false;
+    player1.trailLength = 0;
 
     player2.position = (Vector2){500, 800};
-
-    player1.triangle.height = 20;
-
     player2.triangle.height = 20;
-
-    player1.triangle.width = 40;
-
     player2.triangle.width = 40;
-
-    player1.triangle.rotation = 0;
-
     player2.triangle.rotation = 0;
-
-    player1.state = false;
-
     player2.state = false;
+    player2.trailLength = 0;               
 
     for (int i = 0; i < player1.trailLength; i++) {
         player1.trail[i] = (Vector2){0, 0}; 
@@ -75,46 +69,36 @@ void restartPlayer(){
     for (int i = 0; i < player2.trailLength; i++) {
         player2.trail[i] = (Vector2){0, 0}; 
     }
-    player1.trailLength = 0;
 
-    player2.trailLength = 0;               
 }
 
 void initStage(){
     if (!(player1.state || player2.state)) {
-
-
         movePlayerPos(&player1);
         movePlayerPos(&player2);
-
         initRender(&player1, &player2);
 
         DrawText(TextFormat("Player1 score: %d", player1.score), 1, 1, 20, RED);
-
         DrawText(TextFormat("Player2 score: %d", player2.score), 820, 1, 20, BLUE);
+
     } else {
-
         if (player2.state && player1.state) {
-
             DrawText(TextFormat("Both players died!"), 300, 500, 20, RED);
-
             player1.score += 1;
             player2.score += 1;
 
         } else if (player1.state){
-
-
             //Sleep or anything t2o show who won and restart
             DrawText(TextFormat("Player 1 won!"), 300, 500, 20, RED);
-
             player2.score += 1;
 
         } else if (player2.state){
-
             //Sleep or anything to show who won and restart
             DrawText(TextFormat("Player 2 won!"), 300, 500, 20, RED);
             player1.score += 1;
+
         }
+
         gameOver = true;
         gameStarted = false;
     }
@@ -123,20 +107,18 @@ void initStage(){
 void initController(Texture2D texture) {
     if (!gameStarted && !gameOver) {
         DrawText(TextFormat("Press space to start!"), 200, 500, 60, GREEN);
-
         DrawTexture(texture, -15, -300, WHITE);
 
         if (IsKeyPressed(KEY_SPACE)) {
             gameStarted = true;
         }
+
     } else if (!gameStarted && gameOver){
         DrawText(TextFormat("Press R to restart!"),300, 500, 50, BLUE);
-
         printf("player 1: %d\n", player1.state);
         printf("player 2: %d\n", player2.state);
 
         if (IsKeyPressed(KEY_R)) {
-
             //Restart game
             restartPlayer();
             gameOver = false;
@@ -144,6 +126,7 @@ void initController(Texture2D texture) {
         }
     } else if(gameStarted){
         initStage();
+
     }
 }
 
