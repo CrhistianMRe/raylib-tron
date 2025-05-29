9f0448ecd3152f9c62459eca5ee0872abf0eec4d
CrhistianMRe <came.29meca@gmail.com>
2025-05-29 19:33:01 +0800

    feat(triangle.h): add triangle header file

---
+++ tron/include/triangle.h
@@ -0,0 +1,15 @@
+#ifndef _TRIANGLE_H 
+#define _TRIANGLE_H 
+#include "raylib.h" 
+
+#define VERTEX_AMOUNT 4
+typedef struct Triangle{
+    Vector2 position;
+    long int height;
+    long int width;
+    float rotation;
+    int vertex[VERTEX_AMOUNT];
+} Triangle;
+
+
+#endif // 
