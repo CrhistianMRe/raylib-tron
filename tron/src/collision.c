2602d7b921509afdf047e27e5b363df527bef856
CrhistianMRe <came.29meca@gmail.com>
2025-05-29 20:23:59 +0800

    feat(collision.c): update current vertex implementation to player instace vertex implementation

--- tron/src/collision.c
+++ tron/src/collision.c
@@ -16,10 +16,10 @@ int checkTriangle(Vector2 vertexP1[], Vector2 vertexP2[], Player *player){
 
     
     for(int i = 0; i < 3; i++) {
-        if (CheckCollisionPointTriangle(vertexP1[i], vertexP2[0], vertexP2[1], vertexP2[2])) {
+        if (CheckCollisionPointTriangle(player->triangle.vertex[i], vertexP2[0], vertexP2[1], vertexP2[2] )) {
             player->state= true;
         }
-        if (CheckCollisionPointTriangle(vertexP2[i], vertexP1[0], vertexP1[1], vertexP1[2])) {
+        if (CheckCollisionPointTriangle(player->triangle.vertex[i], vertexP1[0], vertexP1[1], vertexP1[2])) {
             player->state= true;
         }
     }
@@ -34,33 +34,33 @@ int checkCollision(Player *player1, Player *player2, Vector2 trailFix[], Vector2
 
     //Checkear la punta player 2 cuando pega jugador 1
     for (long int a= 0;a < player1->trailLength; a++) {
-        if ((player1->trail[a].x == vertexP2[0].x && player1->trail[a].y == vertexP2[0].y) && player1->state == false) {
+        if ((player1->trail[a].x == player2->triangle.vertex[0].x && player1->trail[a].y == player2->triangle.vertex[0].y) && player1->state == false) {
             player2->state = true;
         }
         //Punta pega a el mismo player 1
-        if ((player1->trail[a].x == vertexP1[0].x && player1->trail[a].y == vertexP1[0].y) && player1->state == false) {
+        if ((player1->trail[a].x == player1->triangle.vertex[0].x && player1->trail[a].y == player1->triangle.vertex[0].y) && player1->state == false) {
             player1->state = true;
         }
     }
     //Checkear la punta creo cuando pega jugador 2
     for (long int a= 0 ;a < player2->trailLength; a++) {
-        if ((player2->trail[a].x == vertexP1[0].x && player2->trail[a].y == vertexP1[0].y)&& player2->state == false) {
+        if ((player2->trail[a].x == player1->triangle.vertex[0].x && player2->trail[a].y == player1->triangle.vertex[0].y)&& player2->state == false) {
             player1->state = true;
 
         }
         //Punto pega a el mismo 2
-        if ((player2->trail[a].x == vertexP2[0].x && player2->trail[a].y == vertexP2[0].y)&& player2->state == false) {
+        if ((player2->trail[a].x == player2->triangle.vertex[0].x && player2->trail[a].y == player2->triangle.vertex[0].y)&& player2->state == false) {
             player2->state = true;
 
         }
     }
-    checkTriangle(vertexP1, vertexP2, player1);
+    checkTriangle(player1->triangle.vertex, player2->triangle.vertex, player1);
 
-    checkTriangle(vertexP1, vertexP2, player2);
+    checkTriangle(player1->triangle.vertex, player2->triangle.vertex, player2);
 
-    checkBorders(player1, vertexP1[0]);
+    checkBorders(player1, player1->triangle.vertex[0]);
 
-    checkBorders(player2, vertexP2[0]);
+    checkBorders(player2, player2->triangle.vertex[0]);
 
     
   return 0;
