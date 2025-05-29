3795edc9a24c049edaac2e8b041cba5242913843
CrhistianMRe <came.29meca@gmail.com>
2025-05-29 16:03:51 +0800

    fix(controller.c): add missing curly brace

--- tron/src/controller.c
+++ tron/src/controller.c
@@ -10,7 +10,7 @@ static bool gameOver = false;
 int speed = 2;
 
 Triangle player1 = {(Vector2){500, 200}, 20, 40, RED, false, 0, 0, {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT}};
-Triangle player2 = {(Vector2){500, 800}, 20, 40, BLUE, false, 0, 0, {KEY_W, KEY_S, KEY_A, KEY_D});
+Triangle player2 = {(Vector2){500, 800}, 20, 40, BLUE, false, 0, 0, {KEY_W, KEY_S, KEY_A, KEY_D}};
 
 int scoreP1 = 0; 
 int scoreP2 = 0; 
