cd41b16a3a44b7507cd00e41a399e09437be5952
CrhistianMRe <came.29meca@gmail.com>
2025-05-29 17:08:51 +0800

    feat(controller.c): change speed to const

--- tron/src/controller.c
+++ tron/src/controller.c
@@ -7,7 +7,7 @@
 static bool gameStarted = false;
 static bool gameOver = false;
 
-int speed = 2;
+const int speed = 2;
 
 Player player1 = {(Vector2){500, 200}, 20, 40, RED, false, 0, 0,{},{KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT}, 0};
 Player player2 = {(Vector2){500, 800}, 20, 40, BLUE, false, 0, 0,{}, {KEY_W, KEY_S, KEY_A, KEY_D}, 0};
